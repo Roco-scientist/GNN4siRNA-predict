d83d09f8d9ff2372a149d7ded3bca288aeedd284
Rory Coffey <coffeyrt@gmail.com>
2025-02-19 20:05:35 -0500

    refactoring

--- predict.py
+++ predict.py
@@ -123,12 +123,12 @@ class kmer_featurization:
         return numbering
 
 
-def build_kmers(sequence, ksize):
+def build_kmers(sequence):
     kmers = []
     n_kmers = len(sequence) - 1
 
     for i in range(n_kmers):
-        kmer = sequence[i : i + ksize]
+        kmer = sequence[i : i + 2]
         kmers.append(kmer)
     return kmers
 
@@ -348,6 +348,49 @@ def find_rnaup(seq):
         return None
 
 
+def cal_thermo_feature(
+    sequence, intermolecular_initiation=4.09, symmetry_correction=0.43
+):
+    sum_stability = 0
+    single_sum = []
+
+    bimers = build_kmers(sequence)
+
+    if sequence[0] == "A":
+        sum_stability += 0.45
+    if sequence[18] == "U":
+        sum_stability += 0.45
+
+    for b in bimers:
+        bimer_values = {
+            "AA": -0.93,
+            "UU": -0.93,
+            "AU": -1.10,
+            "UA": -1.33,
+            "CU": -2.08,
+            "AG": -2.08,
+            "CA": -2.11,
+            "UG": -2.11,
+            "GU": -2.24,
+            "AC": -2.24,
+            "GA": -2.35,
+            "UC": -2.35,
+            "CG": -2.36,
+            "GG": -3.26,
+            "CC": -3.26,
+            "GC": -3.42,
+        }
+        stability_value = bimer_values.get(b, 0)
+        single_sum.append(stability_value)
+        sum_stability += stability_value
+
+    sum_stability += intermolecular_initiation
+    sum_stability += symmetry_correction
+    single_sum.append(sum_stability)
+
+    return single_sum
+
+
 def main():
     k_sirna = 3
     k_mrna = 4
@@ -390,60 +433,12 @@ def main():
         k.insert(0, str(rna_id))
         sirna_kmers_data.append(k)
 
-        sum = 0
-        single_sum = []
-
         sequence = seq_record.seq
         sequence = sequence[:19]
         seq_id = seq_record.id
-        bimer = build_kmers(sequence, 2)
-        single_sum.append(seq_id)
 
-        # checking if first is 'A' and last is 'U'
-        if sequence[0] == "A":
-            sum += +0.45
-        if sequence[18] == "U":
-            sum += +0.45
-
-        # checking the kind of bimers
-        for b in bimer:
-            if b == "AA" or b == "UU":
-                single_sum.append(-0.93)
-                sum += -0.93
-            elif b == "AU":
-                single_sum.append(-1.10)
-                sum += -1.10
-            elif b == "UA":
-                single_sum.append(-1.33)
-                sum += -1.33
-            elif b == "CU" or b == "AG":
-                single_sum.append(-2.08)
-                sum += -2.08
-            elif b == "CA" or b == "UG":
-                single_sum.append(-2.11)
-                sum += -2.11
-            elif b == "GU" or b == "AC":
-                single_sum.append(-2.24)
-                sum += -2.24
-            elif b == "GA" or b == "UC":
-                single_sum.append(-2.35)
-                sum += -2.35
-            elif b == "CG":
-                single_sum.append(-2.36)
-                sum += -2.36
-            elif b == "GG" or b == "CC":
-                single_sum.append(-3.26)
-                sum += -3.26
-            elif b == "GC":
-                single_sum.append(-3.42)
-                sum += -3.42
-            else:
-                single_sum.append(0)
-                sum += 0
-
-        sum += intermolecular_initiation
-        sum += simmetry_correction
-        single_sum.append(sum)
+        single_sum = cal_thermo_feature(sequence)
+        single_sum.insert(0, seq_id)
         total_compute.append(single_sum)
 
         sir = seq_record.seq.upper().replace("U", "T")
