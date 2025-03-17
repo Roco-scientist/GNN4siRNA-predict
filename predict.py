310fdfbc8db75c5c418a6637033c7c7b1d59dd4d
Rory Coffey <coffeyrt@gmail.com>
2025-03-17 10:40:01 -0400

    refactored

--- predict.py
+++ predict.py
@@ -102,8 +102,18 @@ def create_generator(thermo_feats_pd, sirna_pd, mrna_pd):
 
 
 def generate_model(
-    x_train, y_train, x_validate, y_validate, x_hold_out=None, y_hold_out=None, x_predict=None
+    x_train,
+    y_train,
+    x_validate,
+    y_validate,
+    x_hold_out=None,
+    y_hold_out=None,
+    x_predict=None,
 ):
+    """
+    Generates the GNN model and predicts on x_hold_out or x_predict.  For the former it is during
+    evaluation mode.
+    """
     # k-mers of siRNA sequences
     sirna_kmer_data = x_train["siRNA_kmers"]
     sirna_kmer_data.extend(x_validate["siRNA_kmers"])
@@ -111,33 +121,45 @@ def generate_model(
     mrna_kmer_data = x_train["mRNA_kmers"]
     mrna_kmer_data.extend(x_validate["mRNA_kmers"])
 
-    thermo_pd = pd.concat([x_train["Thermo_Features"], x_validate["Thermo_Features"]], ignore_index=True, sort=False)
+    thermo_pd = pd.concat(
+        [x_train["Thermo_Features"], x_validate["Thermo_Features"]],
+        ignore_index=True,
+        sort=False,
+    )
+
+    # If we are doing hold-out cross validation, add these values for the genorator
     if x_hold_out is not None:
         sirna_kmer_data.extend(x_hold_out["siRNA_kmers"])
         mrna_kmer_data.extend(x_hold_out["mRNA_kmers"])
-        thermo_pd = pd.concat([thermo_pd, x_hold_out["Thermo_Features"]], ignore_index=True, sort=False)
+        thermo_pd = pd.concat(
+            [thermo_pd, x_hold_out["Thermo_Features"]], ignore_index=True, sort=False
+        )
 
     if x_predict is not None:
         sirna_kmer_data.extend(x_predict["siRNA_kmers"])
         mrna_kmer_data.extend(x_predict["mRNA_kmers"])
-        thermo_pd = pd.concat([thermo_pd, x_predict["Thermo_Features"]], ignore_index=True, sort=False)
+        thermo_pd = pd.concat(
+            [thermo_pd, x_predict["Thermo_Features"]], ignore_index=True, sort=False
+        )
 
     sirna_pd = pd.DataFrame(data=sirna_kmer_data).drop_duplicates(subset=[0])
     sirna_pd = sirna_pd.set_index(0)
     # k-mers of mRNA sequences
     mRNA_pd = pd.DataFrame(data=mrna_kmer_data).drop_duplicates(subset=[0])
     mRNA_pd = mRNA_pd.set_index(0)
-    generator = create_generator(
-        thermo_pd, sirna_pd, mRNA_pd
-    )
+    generator = create_generator(thermo_pd, sirna_pd, mRNA_pd)
 
     interactions_train = list(
-            x_train["Thermo_Features"].iloc[:, 0].astype(str) + "_" + x_train["Thermo_Features"].iloc[:,1]
+        x_train["Thermo_Features"].iloc[:, 0].astype(str)
+        + "_"
+        + x_train["Thermo_Features"].iloc[:, 1]
     )
     train_gen = generator.flow(interactions_train, y_train, shuffle=True)
 
     interactions_validate = list(
-            x_validate["Thermo_Features"].iloc[:, 0].astype(str) + "_" + x_validate["Thermo_Features"].iloc[:,1]
+        x_validate["Thermo_Features"].iloc[:, 0].astype(str)
+        + "_"
+        + x_validate["Thermo_Features"].iloc[:, 1]
     )
     validate_gen = generator.flow(interactions_validate, y_validate, shuffle=False)
 
@@ -164,8 +186,12 @@ def generate_model(
     # Train the model, keeping track of its loss and accuracy on the training set,
     # and its generalisation performance on the test set
 
-    reduce_lr = ReduceLROnPlateauWithBestWeights(monitor="val_loss", factor=0.5, patience=10)
-    early_stop = callbacks.EarlyStopping(monitor="val_loss", patience=15, restore_best_weights=True)
+    reduce_lr = ReduceLROnPlateauWithBestWeights(
+        monitor="val_loss", factor=0.5, patience=10
+    )
+    early_stop = callbacks.EarlyStopping(
+        monitor="val_loss", patience=15, restore_best_weights=True
+    )
     model.fit(
         train_gen,
         epochs=1000,
@@ -177,7 +203,9 @@ def generate_model(
 
     if ARGS.evaluate:
         interactions_hold_out = list(
-                x_hold_out["Thermo_Features"].iloc[:, 0].astype(str) + "_" + x_hold_out["Thermo_Features"].iloc[:,1]
+            x_hold_out["Thermo_Features"].iloc[:, 0].astype(str)
+            + "_"
+            + x_hold_out["Thermo_Features"].iloc[:, 1]
         )
         hold_out_gen = generator.flow(interactions_hold_out, shuffle=False)
 
@@ -200,7 +228,9 @@ def generate_model(
         return r2, mse, pcc, r2_val, mse_val, pcc_val
     if x_predict is not None:
         interactions_predict = list(
-                x_predict["Thermo_Features"].iloc[:, 0].astype(str) + "_" + x_predict["Thermo_Features"].iloc[:,1]
+            x_predict["Thermo_Features"].iloc[:, 0].astype(str)
+            + "_"
+            + x_predict["Thermo_Features"].iloc[:, 1]
         )
         pred_gen = generator.flow(interactions_predict, shuffle=False)
         pred = model.predict(pred_gen)
@@ -279,10 +309,129 @@ class ReduceLROnPlateauWithBestWeights(callbacks.ReduceLROnPlateau):
             self.model.set_weights(self.best_weights)
 
 
-
 def create_thermo_pd_row(sirna_mrna):
     sirna, mrna = sirna_mrna
     return sirna.thermo_features(mrna)
+
+
+def evaluate_model(
+    thermo_features_file, siRNA_antisense_fasta_file, mRNA_fasta_file, efficacy_file
+):
+    x, y, split_indexes, sirna_mrna = get_split_data(
+        thermo_features_file,
+        siRNA_antisense_fasta_file,
+        mRNA_fasta_file,
+        efficacy_file,
+        folds=6,
+        by_gene=not ARGS.random_split,
+    )
+    if ARGS.random_split:
+        results_file_path = Path("./accuracy.random.csv")
+    else:
+        results_file_path = Path("./accuracy.gene.csv")
+
+    with open(results_file_path, "w") as results_file:
+        results_file.write(
+            "HoldOut,Validation,R2_HoldOut,MSE_HoldOut,PCC_HoldOut,R2_Validation,MSE_Validation,PCC_Validation "
+        )
+
+    for hold_out_set, hold_out_indexes in enumerate(split_indexes):
+        for validation_set, validation_indexes in enumerate(split_indexes):
+            if hold_out_set != validation_set:
+                train_indexes = [
+                    not hold_out and not validation
+                    for hold_out, validation in zip(
+                        hold_out_indexes, validation_indexes
+                    )
+                ]
+                x_train, x_validate, x_hold_out = (
+                    index_x(x, train_indexes, sirna_mrna),
+                    index_x(x, validation_indexes, sirna_mrna),
+                    index_x(x, hold_out_indexes, sirna_mrna),
+                )
+                y_train, y_validate, y_hold_out = (
+                    y[train_indexes],
+                    y[validation_indexes],
+                    y[hold_out_indexes],
+                )
+
+                r2, mse, pcc, r2_val, mse_val, pcc_val = generate_model(
+                    x_train, y_train, x_validate, y_validate, x_hold_out, y_hold_out
+                )
+
+                with open(results_file_path, "a") as results_file:
+                    results_file.write(
+                        f"{hold_out_set + 1},{validation_set + 1},{r2},{mse},{pcc},{r2_val},{mse_val},{pcc_val}"
+                    )
+
+
+def prepare_prediction_data():
+    if ARGS.test:
+        return (
+            Path("./data/raw/test/mRNA_1.fas"),
+            Path("./data/raw/test/sirna_1.fas"),
+            Path("./data/raw/test/sirna_mrna_efficacy.csv"),
+        )
+    if not ARGS.mrna_fasta or not ARGS.sirna_fasta or not ARGS.sirna_mrna_csv:
+        raise ValueError(
+            "mrna_fasta, sirna_fasta, and sirna_mrna_csv inputs are required"
+        )
+    return Path(ARGS.mrna_fasta), Path(ARGS.sirna_fasta), Path(ARGS.sirna_mrna_csv)
+
+
+def load_sequence_data(sirna_fasta_file, mrna_fasta_file):
+    sirna_records, sirna_kmers_data = {}, []
+    for seq_record in SeqIO.parse(sirna_fasta_file, "fasta"):
+        sirna = siRNA(seq_record)
+        sirna_records[sirna.id] = sirna
+        sirna_kmers_data.append(sirna.kmer_data())
+
+    mrna_records, mrna_kmers_data = {}, []
+    for seq_record in SeqIO.parse(mrna_fasta_file, "fasta"):
+        mrna = mRNA(seq_record)
+        mrna_records[mrna.id] = mrna
+        mrna_kmers_data.append(mrna.kmer_data())
+
+    return sirna_records, sirna_kmers_data, mrna_records, mrna_kmers_data
+
+
+def predict_efficacy(
+    thermo_features_file, siRNA_antisense_fasta_file, mRNA_fasta_file, efficacy_file
+):
+    mrna_fasta_file, sirna_fasta_file, sirna_mrna_csv_file = prepare_prediction_data()
+    source_target = pd.read_csv(sirna_mrna_csv_file)
+
+    sirna_records, sirna_kmers_data, mrna_records, mrna_kmers_data = load_sequence_data(
+        sirna_fasta_file, mrna_fasta_file
+    )
+
+    sirna_mrna = [
+        (sirna_records[sirna_id], mrna_records[mrna_id])
+        for sirna_id, mrna_id in zip(source_target.iloc[:, 0], source_target.iloc[:, 1])
+    ]
+
+    print("Finding thermo properties. This may take a while")
+    with Pool(ARGS.threads) as pool:
+        thermo_rows = pool.map(create_thermo_pd_row, sirna_mrna)
+
+    x_train, y_train, x_validate, y_validate = get_split_data_gene_model(
+        thermo_features_file, siRNA_antisense_fasta_file, mRNA_fasta_file, efficacy_file
+    )
+
+    x_predict = {
+        "Thermo_Features": pd.DataFrame(thermo_rows),
+        "siRNA_kmers": pd.DataFrame(sirna_kmers_data),
+        "mRNA_kmers": pd.DataFrame(mrna_kmers_data),
+    }
+
+    source_target["Efficacy_Prediction"] = generate_model(
+        x_train, y_train, x_validate, y_validate, x_predict=x_predict
+    )
+
+    if not ARGS.test:
+        source_target.to_csv(
+            sirna_mrna_csv_file.with_suffix(".prediction.csv"), index=False
+        )
 
 
 def main():
@@ -290,108 +439,21 @@ def main():
     siRNA_antisense_fasta_file = Path("./data/raw/HUVKS/siRNA.fa")
     mRNA_fasta_file = Path("./data/raw/HUVKS/mrna.fa")
     efficacy_file = Path("./data/raw/HUVKS/sirna_mrna_efficacy.csv")
+
     if ARGS.evaluate:
-        x, y, split_indexes, sirna_mrna = get_split_data(
+        evaluate_model(
             thermo_features_file,
             siRNA_antisense_fasta_file,
             mRNA_fasta_file,
             efficacy_file,
-            folds=6,
-            by_gene= not ARGS.random_split,
         )
-        results_file_path = Path("./accuracy.csv")
-        with open(results_file_path, "w") as results_file:
-            results_file.write(
-                "HoldOut,Validation,R2_HoldOut,MSE_HoldOut,PCC_HoldOut,R2_Validation,MSE_Validation,PCC_Validation\n"
-            )
-        for hold_out_set in range(len(split_indexes)):
-            for validation_set in range(len(split_indexes)):
-                if hold_out_set != validation_set:
-                    hold_out_indexes = split_indexes[hold_out_set]
-                    validation_indexes = split_indexes[validation_set]
-                    train_indexes = [
-                        not hold_out and not validation
-                        for hold_out, validation in zip(
-                            hold_out_indexes, validation_indexes
-                        )
-                    ]
-                    x_train = index_x(x, train_indexes, sirna_mrna)
-                    x_validate = index_x(x, validation_indexes, sirna_mrna)
-                    x_hold_out = index_x(x, hold_out_indexes, sirna_mrna)
-
-                    y_train = y[train_indexes]
-                    y_validate = y[validation_indexes]
-                    y_hold_out = y[hold_out_indexes]
-
-                    r2, mse, pcc, r2_val, mse_val, pcc_val = generate_model(
-                        x_train, y_train, x_validate, y_validate, x_hold_out, y_hold_out
-                    )
-                    with open(results_file_path, "a") as results_file:
-                        results_file.write(
-                            f"{hold_out_set + 1},{validation_set + 1},{r2},{mse},{pcc},{r2_val},{mse_val},{pcc_val}\n"
-                        )
     else:
-        x_predict = {}
-        if ARGS.test:
-            mrna_fasta_file = Path("./data/raw/test/mRNA_1.fas")
-            sirna_fasta_file = Path("./data/raw/test/sirna_1.fas")
-            sirna_mrna_csv_file = Path("./data/raw/test/sirna_mrna_efficacy.csv")
-        else:
-            if ARGS.mrna_fasta is None:
-                raise ValueError("mrna_fasta input required")
-            if ARGS.sirna_fasta is None:
-                raise ValueError("sirna_fasta input required")
-            if ARGS.sirna_mrna_csv is None:
-                raise ValueError("sirna_mrna_csv input required")
-            mrna_fasta_file = Path(ARGS.mrna_fasta)
-            sirna_fasta_file = Path(ARGS.sirna_fasta)
-            sirna_mrna_csv_file = Path(ARGS.sirna_mrna_csv)
-        source_target = pd.read_csv(sirna_mrna_csv_file)
-
-        sequence_info = []
-        # create sirna k-mer file
-        sirna_records = {}
-        sirna_kmers_data = []
-        for seq_record in SeqIO.parse(sirna_fasta_file, "fasta"):  # input fasta file
-            sirna = siRNA(seq_record)
-            sirna_records[sirna.id] = sirna
-            sirna_kmers_data.append(sirna.kmer_data())
-
-        mrna_records = {}
-        mrna_kmers_data = []
-        for seq_record in SeqIO.parse(mrna_fasta_file, "fasta"):
-            mrna = mRNA(seq_record)
-            mrna_records[mrna.id] = mrna
-            mrna_kmers_data.append(mrna.kmer_data())
-
-        sirna_mrna = [
-            (sirna_records[sirna_id], mrna_records[mrna_id])
-            for sirna_id, mrna_id in zip(
-                source_target.iloc[:, 0], source_target.iloc[:, 1]
-            )
-        ]
-        print("\nFinding thermo properties.  May take awhile\n")
-        with Pool(ARGS.threads) as pool:
-            thermo_rows = pool.map(create_thermo_pd_row, sirna_mrna)
-
-
-        x_train, y_train, x_validate, y_validate = get_split_data_gene_model(
+        predict_efficacy(
             thermo_features_file,
             siRNA_antisense_fasta_file,
             mRNA_fasta_file,
             efficacy_file,
         )
-        x_predict["Thermo_Features"] = pd.DataFrame(thermo_rows)
-        x_predict["siRNA_kmers"] = pd.DataFrame(sirna_kmers_data)
-        x_predict["mRNA_kmers"] = pd.DataFrame(mrna_kmers_data)
-        source_target["Efficacy_Prediction"] = generate_model(
-            x_train, y_train, x_validate, y_validate, x_predict=x_predict
-        )
-
-        if not ARGS.test:
-            source_target.to_csv(
-                sirna_mrna_csv_file.with_suffix(".prediction.csv"), index=False
-            )
 
 
 if __name__ == "__main__":
